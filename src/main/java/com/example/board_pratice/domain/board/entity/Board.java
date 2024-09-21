9dd0a76f070f03c81c6cbdfa6654cae716ea559f
Kyuwon-Choi <kyuwon30@naver.com>
2024-09-21 13:45:58 +0900

    add: 엔티티 작성

--- src/main/java/com/example/board_pratice/domain/board/entity/Board.java
+++ src/main/java/com/example/board_pratice/domain/board/entity/Board.java
@@ -1,14 +1,24 @@
 package com.example.board_pratice.domain.board.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 @Entity
-@Data
 public class Board {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
+
+    public Board(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Board() {
+    }
 }
--- src/main/java/com/example/board_pratice/domain/board/entity/Comment.java
+++ src/main/java/com/example/board_pratice/domain/board/entity/Comment.java
@@ -1,4 +1,24 @@
 package com.example.board_pratice.domain.board.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private Long boardId;
+
+    public Comment(Long id, String content, Long boardId) {
+        this.id = id;
+        this.content = content;
+        this.boardId = boardId;
+    }
+
+    public Comment() {
+    }
 }
